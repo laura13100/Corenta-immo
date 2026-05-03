434704a86d3692b0c4680b27af13f5a03920ddc8
laura13100 <kewerolle@gmail.com>
2026-05-03 09:44:51 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://rinotvlhnvgrszdpkcw.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_txSvyD7f6jisZE3huKbWRg__uf5m9AX'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+export const supabase = createClient(
+  'https://rinotvlhnvgrszdpkcw.supabase.co',
+  'sb_publishable_txSvyD7f6jisZE3huKbWRg__uf5m9AX',
+  {
+    auth: {
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: true
+    }
+  }
+)
